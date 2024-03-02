565536f7da90cbb0eeb65e8af1bde3b23fa8730d
HenriqueSilvaXavier <134643923+HenriqueSilvaXavier@users.noreply.github.com>
2024-03-02 20:11:11 -0300

    Add files via upload

---
+++ mix().py
@@ -0,0 +1,16 @@
+mport random
+lista=["Henrique", "Gian", "Mateus", "André", "Leonardo", "Carlos", "Felipe", "Ricardo"]
+n=7
+def mix(lista, n):
+	c=1
+	while len(lista)>0:
+		grupo=[]
+		if len(lista)<n:
+			n=len(lista)
+		for k in range(0, n):
+			e=random.choice(lista)
+			lista.remove(e)
+			grupo.append(e)
+		print("Grupo {}: {}".format(c, grupo))
+		c=c+1
+mix(lista, n)
